cf1c024de94f38879a48fc06164157b3b3639197
Christopher C. Wells <chris@chrxs.net>
2019-04-06 10:40:10 -0700

    Install AdminLTE base package.

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace CascadePublicMedia\PbsApiExplorer\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BaseController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('base.html.twig', [
+            'controller_name' => 'BaseController',
+        ]);
+    }
+}
