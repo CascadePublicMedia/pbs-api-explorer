9d8f120cc924d86b90a51bc378ce6c3d4770f4b6
Christopher C. Wells <chris@chrxs.net>
2019-04-11 19:20:54 -0700

    Add option to sync from public or internal API for Station Manager.

--- src/Controller/StationManagerController.php
+++ src/Controller/StationManagerController.php
@@ -42,7 +42,7 @@ class StationManagerController extends ControllerBase
      */
     public function stations(EntityManagerInterface $entityManager) {
         $entities = $entityManager->getRepository(Station::class)->findAll();
-        return $this->render('datatable.html.twig', [
+        return $this->render('station_manager/datatable.html.twig', [
             'title' => 'Stations',
             'properties' => [
                 'fullCommonName' => 'Name',
@@ -53,7 +53,8 @@ class StationManagerController extends ControllerBase
                 'updated' => 'Updated',
             ],
             'entities' => $entities,
-            'update_route' => 'station_manager_stations_update',
+            'update_route_internal' => 'station_manager_stations_update',
+            'update_route_public' => 'station_manager_stations_public_update',
         ]);
     }
 
