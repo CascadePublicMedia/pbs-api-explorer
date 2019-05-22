45cbb4737c8414820d512aa3c4859a674a415a2d
Christopher C. Wells <chris@chrxs.net>
2019-05-22 13:47:49 -0700

    Add RepositoryBase method to eager-load an entity and all associations.

--- src/Repository/RepositoryBase.php
+++ src/Repository/RepositoryBase.php
@@ -3,6 +3,9 @@
 namespace CascadePublicMedia\PbsApiExplorer\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
+use Doctrine\ORM\Mapping\MappingException;
+use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\ORM\Query\QueryException;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
@@ -27,5 +30,33 @@ class RepositoryBase extends ServiceEntityRepository
         $qb = $this->createQueryBuilder('s');
         $query = $qb->indexBy('s', 's.id')->getQuery();
         return $query->getResult();
+    }
+
+    /**
+     * Retrieve a single entity by ID with all associations eager loading.
+     *
+     * @param $id
+     *
+     * @return mixed
+     *
+     * @throws MappingException
+     * @throws NonUniqueResultException
+     */
+    public function findEager($id) {
+        $query = $this->createQueryBuilder('t')
+            ->andWhere('t.id = :id')
+            ->setParameter('id', $id)
+            ->getQuery();
+
+        $metadata = $this->getClassMetadata();
+        $associations = $metadata->getAssociationMappings();
+        foreach ($associations as $association) {
+            $metadata->setAssociationOverride(
+                $association['fieldName'],
+                ['fetch' => ClassMetadataInfo::FETCH_EAGER]
+            );
+        }
+
+        return $query->getOneOrNullResult();
     }
 }
